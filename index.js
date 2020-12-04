ec0e69609c4d96cd27cb9b327ca2eebda03c962e
Robert <rbbenett0614@gmail.com>
2020-12-04 20:14:17 +0000

    Final Commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
-const taskDescriptions = [];
+// // Arrays to keep track of each task's state
+// const taskTitles = [];
+// const taskComplete = [];
+// const taskDescriptions = [];
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
@@ -9,21 +9,18 @@ function newTask(title, description) {
   const task = {
     title: title,
     description: description,
-    complete: false
-  };
-  return task;
-}
+    complete: false,
 
 
-// Print the state of a task to the console in a nice readable way
-function logTaskState(task) {
-  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
-}
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
 
-
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(task) {
-  task.complete = true;
+    markCompleted: function() {
+      this.complete = true;
+   }
+ };
+ return task;
 }
 
 
@@ -33,4 +30,6 @@ const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter b
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
-console.log(tasks);
+task1.logState();
+task1.markCompleted();
+task1.logState();
