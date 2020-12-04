d7a9cce41e23be32125a4b1a7a471bfaa5a4565f
Robert <rbbenett0614@gmail.com>
2020-12-04 20:07:51 +0000

    Fourth Commit

--- index.js
+++ index.js
@@ -14,17 +14,18 @@ function newTask(title, description) {
   return task;
 }
 
-// // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-// function completeTask(taskIndex) {
-//   taskComplete[taskIndex] = true;
-// }
 
-// // Print the state of a task to the console in a nice readable way
-// function logTaskState(taskIndex) {
-//   const title = taskTitles[taskIndex];
-//   const complete = taskComplete[taskIndex];
-//   console.log(`${title} has${complete ? " " : " not "}been completed`);
-// }
+// Print the state of a task to the console in a nice readable way
+function logTaskState(task) {
+  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+}
+
+
+// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
+function completeTask(task) {
+  task.complete = true;
+}
+
 
 // DRIVER CODE BELOW
 
